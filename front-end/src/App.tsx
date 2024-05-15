f65fe79fc7b685439174873ff6406d07b0e1d6b8
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-05-15 13:13:05 -0500

    get functionality implementation for players

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,47 @@
 import './App.css'
-import { Header } from "./components/header_footer/Header.tsx";
+import {Header} from "./components/header_footer/Header.tsx";
 import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
 import Error from "./components/Error.tsx"
 import Teams from "./components/Teams.tsx";
 import Admin from "./components/Admin.tsx";
 import Dashboard from "./components/Dashboard.tsx";
+import {useEffect, useState} from "react";
+import axios, {AxiosResponse} from "axios";
 
-function App() {
+export default function App() {
 
-  return (
-    <>
-        <Router>
+    const [players, setPlayers] = useState<Player[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/player')
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+                setPlayers(response.data)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <>
+            <Router>
                 <Header/>
                 <Routes>
                     <Route path="/" element={<Dashboard/>}/>
-                    <Route path="/teams" element={<Teams/>}/>
+                    <Route path="/teams" element={<Teams players={players}/>}/>
                     <Route path="/admin" element={<Admin/>}/>
                     <Route path="*" element={<Error/>}/>
                 </Routes>
-        </Router>
-    </>
-  )
+            </Router>
+        </>
+    )
 }
 
-export default App
+export type Player = {
+    id: number;
+    puuid: string;
+    summonerId: string;
+    summonerName: string;
+    team: string;
+}
--- front-end/src/components/Teams.tsx
+++ front-end/src/components/Teams.tsx
@@ -1,7 +1,65 @@
-function Teams() {
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Player } from "../App.tsx";
+
+export type PlayerListProps = {
+    players: Player[]
+}
+
+function Teams(players:PlayerListProps) {
+
+    console.log(players)
+
     return (
         <>
             <h1>Teams Page</h1>
+            <Accordion disabled >
+                <AccordionSummary className={'team'}>
+                    <Typography variant={"h5"} >Team 1</Typography>
+                </AccordionSummary>
+            </Accordion>
+
+            {players.players.map((el: Player) => (
+                <Accordion key={el.id}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2-content"
+                        id="panel2-header"
+                    >
+                        {el.summonerName}
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {"Team: " + el.team}
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    Player 1
+                </AccordionSummary>
+                <AccordionDetails>
+                    Rank: Poopy
+                </AccordionDetails>
+            </Accordion>
+
+
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3-content"
+                    id="panel3-header"
+                >
+                    Player 2
+                </AccordionSummary>
+                <AccordionDetails>
+                    Rank: Poopy
+                </AccordionDetails>
+            </Accordion>
         </>
     )
 }
