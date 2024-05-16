61270829311be77512a9c421dc73ac8602017efe
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-05-16 17:18:46 -0500

    update player page and make base post requests for basic data

---
+++ front-end/src/components/PlayerPage.tsx
@@ -0,0 +1,42 @@
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {Player} from "../App.tsx";
+
+type PlayerPageProps = { player: Player };
+
+function PlayerPage(props : PlayerPageProps) {
+
+
+    return (
+        <>
+            <Accordion key={props.player.summonerId}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${props.player.summonerId}-content`} id={`${props.player.summonerId}-header`}>
+                    <Typography>{props.player.summonerName}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Rank: Poopy
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </>
+    )
+}
+
+export default PlayerPage;
+
+type PlayerInfo = {
+    summonerName: string;
+    rank: string;
+    lastTenGames: prevGame[];
+}
+
+type prevGame = {
+    champion: string;
+    win: boolean;
+    lane: string;
+    damage: string;
+    kills: string;
+    deaths: string;
+    assists: string;
+}
--- front-end/src/components/TeamsPage.tsx
+++ front-end/src/components/TeamsPage.tsx
@@ -1,6 +1,7 @@
 import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {Player, Team} from "../App.tsx";
+import PlayerPage from "./PlayerPage.tsx";
 
 type TeamsPageProps = { teams: Team[] };
 
@@ -9,7 +10,6 @@ function TeamsPage(props : TeamsPageProps) {
     return (
         <>
             <h1>Teams Page</h1>
-
             {props.teams.map((team: Team) => (
                 <Accordion key={team.name}>
                     <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${team.name}-content`} id={`${team.name}-header`}>
@@ -18,50 +18,12 @@ function TeamsPage(props : TeamsPageProps) {
                     <AccordionDetails>
                         <div style={{ width: '100%' }}>
                             {team.players.map((player: Player) => (
-                                <Accordion key={player.summonerId}>
-                                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${player.summonerId}-content`} id={`${player.summonerId}-header`}>
-                                        <Typography>{player.summonerName}</Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Typography>
-                                            Rank: Poopy
-                                        </Typography>
-                                    </AccordionDetails>
-                                </Accordion>
+                                <PlayerPage player={player}/>
                             ))}
                         </div>
                     </AccordionDetails>
                 </Accordion>
             ))}
-
-            {/*{players.players.map((el: Player) => (*/}
-            {/*    <Accordion key={el.id}>*/}
-            {/*        <AccordionSummary*/}
-            {/*            expandIcon={<ExpandMoreIcon />}*/}
-            {/*            aria-controls="panel2-content"*/}
-            {/*            id="panel2-header"*/}
-            {/*        >*/}
-            {/*            {el.summonerName}*/}
-            {/*        </AccordionSummary>*/}
-            {/*        <AccordionDetails>*/}
-            {/*            {"Team: " + el.team}*/}
-            {/*        </AccordionDetails>*/}
-            {/*    </Accordion>*/}
-            {/*))}*/}
-
-
-            {/*<Accordion>*/}
-            {/*    <AccordionSummary*/}
-            {/*        expandIcon={<ExpandMoreIcon />}*/}
-            {/*        aria-controls="panel3-content"*/}
-            {/*        id="panel3-header"*/}
-            {/*    >*/}
-            {/*        Hardcoded*/}
-            {/*    </AccordionSummary>*/}
-            {/*    <AccordionDetails>*/}
-            {/*        Rank: Poopy*/}
-            {/*    </AccordionDetails>*/}
-            {/*</Accordion>*/}
         </>
     )
 }
