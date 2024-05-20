39c098b2c40cafc8b5a39ae73f4f635e9e46efa2
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-05-20 11:06:37 -0500

    API calls working for match IDs and gathering match specific information

--- front-end/src/components/PlayerPage.tsx
+++ front-end/src/components/PlayerPage.tsx
@@ -10,6 +10,12 @@ function PlayerPage(props : PlayerPageProps) {
 
     //const {player} = props
     let {id, puuid, summonerId, summonerName, team, tagline} = props.player;
+    let tempInfo : PlayerInfo = {
+        summonerName: summonerName,
+        rank: "temp",
+        lastTwentyGames: [],
+        summonerLevel: "-1",
+    }
 
     async function getAccountInformation() {
         // https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/thebighook/NA1?api_key=***
@@ -20,10 +26,11 @@ function PlayerPage(props : PlayerPageProps) {
             .then((response: AxiosResponse) => {
                 console.log(response.data);
                 puuid = response.data.puuid;
+
                 getSummonerInformation(puuid);
             })
             .catch((error) => {
-                console.error('Error fetching summoner data:', error);
+                console.error('Error fetching summoner data: ', error);
             });
     }
 
@@ -36,11 +43,111 @@ function PlayerPage(props : PlayerPageProps) {
             .then((response: AxiosResponse) => {
                 console.log(response.data);
                 summonerId = response.data.id;
+                tempInfo.summonerLevel = response.data.summonerLevel;
 
+                getLeagueInformation(summonerId);
+                getRankedMatchHistory(init_puuid);
             })
             .catch((error) => {
-                console.error('Error fetching account data:', error);
+                console.error('Error fetching account data: ', error);
             });
+    }
+
+    async function getLeagueInformation(init_id: string) {
+        // /lol/league/v4/entries/by-summoner/{encryptedSummonerId}
+
+        const leagueInfo_url = `/api/get-league-info/${init_id}`
+
+        await axios.get(leagueInfo_url)
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+
+                if (response.data.length === 0)
+                    tempInfo.rank = "No rank available this season"
+                else {
+
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching league data:', error);
+            });
+    }
+
+    async function getRankedMatchHistory(init_puuid: string) {
+
+        const matchInfo_url = `/api/get-matches/${init_puuid}`
+
+        await axios.get(matchInfo_url)
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+
+                tempInfo.lastTwentyGames = response.data;
+                tempInfo.lastTwentyGames.map(el => {
+                    if (el != null)
+                        getMatchInformation(el)
+                    else
+                        console.log("NULL DATA")
+                })
+            })
+            .catch((error) => {
+                console.error('Error fetching match data:', error);
+            });
+    }
+
+    async function getMatchInformation(matchId: string) {
+        const matchId_url = `/api/get-match-info/${matchId}`
+
+        await axios.get(matchId_url)
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+                parseMatchData(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching matchId data:', error);
+            });
+
+    }
+
+    function parseMatchData(matchData : any) {
+
+        let playerNumber = -1;
+
+        let tempGameInfo : GameInfo = {
+            champion: "",
+            win: false,
+            lane: "",
+            kills: -1,
+            deaths: -1,
+            assists: -1,
+        }
+
+        let index = 0;
+        matchData.info.participants.map((player : any) => {
+            if (player.summonerName === tempInfo.summonerName) {
+                playerNumber = index;
+            }
+
+            index++;
+        })
+
+        if (playerNumber != -1) {
+            //     champion: string;
+            tempGameInfo.champion = matchData.info.participants[playerNumber].championName;
+            //     win: boolean;
+            tempGameInfo.win = matchData.info.participants[playerNumber].win;
+            //     lane: number;
+            tempGameInfo.lane = matchData.info.participants[playerNumber].lane;
+            //     kills: number;
+            tempGameInfo.kills = matchData.info.participants[playerNumber].kills;
+            //     deaths: number;
+            tempGameInfo.deaths = matchData.info.participants[playerNumber].deaths;
+            //     assists: number;
+            tempGameInfo.assists = matchData.info.participants[playerNumber].assists;
+
+            console.log(tempGameInfo)
+        } else
+            console.log("ERROR FINDING CHAMPION INFORMATION")
+
     }
 
     //get correct PUUID and summonerID using tagline and summonerName
@@ -56,7 +163,7 @@ function PlayerPage(props : PlayerPageProps) {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Rank: Poopy
+                        Rank: temp
                     </Typography>
                 </AccordionDetails>
             </Accordion>
@@ -66,18 +173,18 @@ function PlayerPage(props : PlayerPageProps) {
 
 export default PlayerPage;
 
-// type PlayerInfo = {
-//     summonerName: string;
-//     rank: string;
-//     lastTenGames: prevGame[];
-// }
-//
-// type prevGame = {
-//     champion: string;
-//     win: boolean;
-//     lane: string;
-//     damage: string;
-//     kills: string;
-//     deaths: string;
-//     assists: string;
-// }
+type PlayerInfo = {
+    summonerName: string;
+    rank: string;
+    lastTwentyGames: [];
+    summonerLevel: string;
+}
+
+type GameInfo = {
+    champion: string;
+    win: boolean;
+    lane: string;
+    kills: number;
+    deaths: number;
+    assists: number;
+}
--- src/main/java/com/example/LoF_Dashboard/RiotProxy/RiotProxyController.java
+++ src/main/java/com/example/LoF_Dashboard/RiotProxy/RiotProxyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class RiotProxyController {
 
-    @Value("${riot.api.key}")
+    @Value("RGAPI-d6660762-3466-4c4c-a432-552e8816b0f2")
     private String apiKey;
 
     private final RestTemplate restTemplate;
@@ -32,4 +32,29 @@ public class RiotProxyController {
                 + puuid + "?api_key=" + apiKey;
         return restTemplate.getForObject(url, Object.class);
     }
+
+    // /lol/league/v4/entries/by-summoner/{encryptedSummonerId}
+    @GetMapping("/api/get-league-info/{id}")
+    public Object getLeagueInformation(@PathVariable String id) {
+        String url = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"
+                + id + "?api_key=" + apiKey;
+        return restTemplate.getForObject(url, Object.class);
+    }
+
+    // https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/DAVg3J8PjIO2EQRWSWRsEDHDvVOKN09yOcB1MgmMMRKgIbeGGYmUR9mt0B_nCp3qFP7d-0MF1O5lpg/ids?type=ranked&start=0&count=20&api_key=
+    @GetMapping("/api/get-matches/{puuid}")
+    public Object getRankedMatchHistory(@PathVariable String puuid) {
+        String url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"
+                + puuid + "/ids?type=ranked&start=0&count=1&api_key=" + apiKey;
+        return restTemplate.getForObject(url, Object.class);
+    }
+
+    //https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4969064550?api_key=
+    @GetMapping("/api/get-match-info/{matchId}")
+    public Object getMatchInformation(@PathVariable String matchId) {
+        String url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
+                + matchId + "?api_key=" + apiKey;
+        return restTemplate.getForObject(url, Object.class);
+    }
+
 }
