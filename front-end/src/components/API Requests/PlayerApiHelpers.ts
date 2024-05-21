04dd69cc38b5982925f7314d15499af6bd914e9b
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-05-21 10:46:49 -0500

    set up admin page for C,U,D of CRUD

--- front-end/src/components/API Requests/PlayerApiHelpers.ts
+++ front-end/src/components/API Requests/PlayerApiHelpers.ts
@@ -1,138 +1,138 @@
-import axios, {AxiosResponse} from "axios";
-import {GameInfo} from "../PlayerGameInfo.tsx";
-import {PlayerInfo} from "../PlayerPage.tsx";
-
-let tempInfo : PlayerInfo = {
-    summonerName: "temp",
-    rank: "temp",
-    prevGames: [],
-    summonerLevel: "-1",
-}
-
-function getAccountInformation(summonerName: string, tagline: string): PlayerInfo | null {
-    // https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/thebighook/NA1?api_key=***
-
-    tempInfo.summonerName = summonerName;
-
-    const puuid_url = `/api/get-account/${summonerName}/${tagline}`;
-
-    axios.get(puuid_url)
-        .then((response: AxiosResponse) => {
-            console.log("Request: getAccountInformation");
-            let puuid = response.data.puuid;
-            getSummonerInformation(puuid);
-        })
-        .catch((error) => {
-            console.error('Error fetching summoner data: ', error);
-        });
-
-    return tempInfo;
-}
-
-function getSummonerInformation(init_puuid: string) {
-    const summonerId_url = `/api/get-summoner/${init_puuid}`;
-
-    axios.get(summonerId_url)
-        .then((response: AxiosResponse) => {
-            console.log("Request: getSummonerInformation");
-            let summonerId = response.data.id;
-            tempInfo.summonerLevel = response.data.summonerLevel;
-            getLeagueInformation(summonerId, init_puuid);
-        })
-        .catch((error) => {
-            console.error('Error fetching account data: ', error);
-        });
-}
-
-function getLeagueInformation(init_id: string, init_puuid: string) {
-    // /lol/league/v4/entries/by-summoner/{encryptedSummonerId}
-
-    const leagueInfo_url = `/api/get-league-info/${init_id}`
-
-    axios.get(leagueInfo_url)
-        .then((response: AxiosResponse) => {
-            console.log("Request: getLeagueInformation");
-            if (response.data.length === 0) {
-                tempInfo.rank = "No rank available this season";
-            } else {
-                tempInfo.rank = "I need to do something about this - you have a rank";
-            }
-            getRankedMatchHistory(init_puuid);
-        })
-        .catch((error) => {
-            console.error('Error fetching league data:', error);
-        });
-}
-
-function getRankedMatchHistory(init_puuid: string) {
-    const matchInfo_url = `/api/get-matches/${init_puuid}`;
-
-    axios.get(matchInfo_url)
-        .then((response: AxiosResponse) => {
-            console.log(response.data);
-
-            tempInfo.prevGames = response.data;
-            // tempInfo.prevGames.map(el => {
-            //     if (el != null)
-            //         getMatchInformation(el)
-            //     else
-            //         console.log("NULL DATA")
-            // })
-
-            // set player info state upon receiving full data
-            // setPlayerInfo(tempInfo);
-        })
-        .catch((error) => {
-            console.error('Error fetching match data:', error);
-        });
-}
-
-async function getMatchInformation(matchId: string) {
-    const matchId_url = `/api/get-match-info/${matchId}`
-
-    await axios.get(matchId_url)
-        .then((response: AxiosResponse) => {
-            console.log("Request: getMatchInformation");
-            parseMatchData(response.data);
-        })
-        .catch((error) => {
-            console.error('Error fetching matchId data:', error);
-        });
-}
-
-function parseMatchData(matchData: any) : GameInfo {
-    let playerNumber = -1;
-
-    const tempGameInfo: GameInfo = {
-        champion: "",
-        win: false,
-        lane: "",
-        kills: -1,
-        deaths: -1,
-        assists: -1,
-    };
-
-    let index = 0;
-    matchData.info.participants.map((player: any) => {
-        if (player.summonerName === tempInfo.summonerName) {
-            playerNumber = index;
-        }
-        index++;
-    });
-
-    if (playerNumber !== -1) {
-        const participant = matchData.info.participants[playerNumber];
-        tempGameInfo.champion = participant.championName;
-        tempGameInfo.win = participant.win;
-        tempGameInfo.lane = participant.lane;
-        tempGameInfo.kills = participant.kills;
-        tempGameInfo.deaths = participant.deaths;
-        tempGameInfo.assists = participant.assists;
-
-        //setGamesInfo(prevItems => [...prevItems, tempGameInfo]);
-    } else {
-        console.log("ERROR FINDING CHAMPION INFORMATION");
-    }
-
-    return tempGameInfo;
-}
+// import axios, {AxiosResponse} from "axios";
+// import {GameInfo} from "../PlayerGameInfo.tsx";
+// import {PlayerInfo} from "../PlayerPage.tsx";
+//
+// let tempInfo : PlayerInfo = {
+//     summonerName: "temp",
+//     rank: "temp",
+//     prevGames: [],
+//     summonerLevel: "-1",
+// }
+//
+// function getAccountInformation(summonerName: string, tagline: string): PlayerInfo | null {
+//     // https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/thebighook/NA1?api_key=***
+//
+//     tempInfo.summonerName = summonerName;
+//
+//     const puuid_url = `/api/get-account/${summonerName}/${tagline}`;
+//
+//     axios.get(puuid_url)
+//         .then((response: AxiosResponse) => {
+//             console.log("Request: getAccountInformation");
+//             let puuid = response.data.puuid;
+//             getSummonerInformation(puuid);
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching summoner data: ', error);
+//         });
+//
+//     return tempInfo;
+// }
+//
+// function getSummonerInformation(init_puuid: string) {
+//     const summonerId_url = `/api/get-summoner/${init_puuid}`;
+//
+//     axios.get(summonerId_url)
+//         .then((response: AxiosResponse) => {
+//             console.log("Request: getSummonerInformation");
+//             let summonerId = response.data.id;
+//             tempInfo.summonerLevel = response.data.summonerLevel;
+//             getLeagueInformation(summonerId, init_puuid);
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching account data: ', error);
+//         });
+// }
+//
+// function getLeagueInformation(init_id: string, init_puuid: string) {
+//     // /lol/league/v4/entries/by-summoner/{encryptedSummonerId}
+//
+//     const leagueInfo_url = `/api/get-league-info/${init_id}`
+//
+//     axios.get(leagueInfo_url)
+//         .then((response: AxiosResponse) => {
+//             console.log("Request: getLeagueInformation");
+//             if (response.data.length === 0) {
+//                 tempInfo.rank = "No rank available this season";
+//             } else {
+//                 tempInfo.rank = "I need to do something about this - you have a rank";
+//             }
+//             getRankedMatchHistory(init_puuid);
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching league data:', error);
+//         });
+// }
+//
+// function getRankedMatchHistory(init_puuid: string) {
+//     const matchInfo_url = `/api/get-matches/${init_puuid}`;
+//
+//     axios.get(matchInfo_url)
+//         .then((response: AxiosResponse) => {
+//             console.log(response.data);
+//
+//             tempInfo.prevGames = response.data;
+//             // tempInfo.prevGames.map(el => {
+//             //     if (el != null)
+//             //         getMatchInformation(el)
+//             //     else
+//             //         console.log("NULL DATA")
+//             // })
+//
+//             // set player info state upon receiving full data
+//             // setPlayerInfo(tempInfo);
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching match data:', error);
+//         });
+// }
+//
+// async function getMatchInformation(matchId: string) {
+//     const matchId_url = `/api/get-match-info/${matchId}`
+//
+//     await axios.get(matchId_url)
+//         .then((response: AxiosResponse) => {
+//             console.log("Request: getMatchInformation");
+//             parseMatchData(response.data);
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching matchId data:', error);
+//         });
+// }
+//
+// function parseMatchData(matchData: any) : GameInfo {
+//     let playerNumber = -1;
+//
+//     const tempGameInfo: GameInfo = {
+//         champion: "",
+//         win: false,
+//         lane: "",
+//         kills: -1,
+//         deaths: -1,
+//         assists: -1,
+//     };
+//
+//     let index = 0;
+//     matchData.info.participants.map((player: any) => {
+//         if (player.summonerName === tempInfo.summonerName) {
+//             playerNumber = index;
+//         }
+//         index++;
+//     });
+//
+//     if (playerNumber !== -1) {
+//         const participant = matchData.info.participants[playerNumber];
+//         tempGameInfo.champion = participant.championName;
+//         tempGameInfo.win = participant.win;
+//         tempGameInfo.lane = participant.lane;
+//         tempGameInfo.kills = participant.kills;
+//         tempGameInfo.deaths = participant.deaths;
+//         tempGameInfo.assists = participant.assists;
+//
+//         //setGamesInfo(prevItems => [...prevItems, tempGameInfo]);
+//     } else {
+//         console.log("ERROR FINDING CHAMPION INFORMATION");
+//     }
+//
+//     return tempGameInfo;
+// }
--- front-end/src/components/Admin.tsx
+++ front-end/src/components/Admin.tsx
@@ -1,9 +1,103 @@
-function Admin() {
-    return (
-        <>
-            <h1>Admin Page</h1>
-        </>
-    )
+
+// function Admin() {
+//
+//
+//     return (
+//         <>
+//             <h1>Admin Page</h1>
+//             {/* add team */}
+//
+//             {/* add player */}
+//
+//             {/* delete player/team */}
+//
+//             {/* change player */}
+//
+//             {/* add week */}
+//
+//             {/* add games to week */}
+//         </>
+//     )
+// }
+//
+// export default Admin;
+
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export default Admin;
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Admin() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+    {/* add team */}
+
+    {/* add player */}
+
+    {/* delete player/team */}
+
+    {/* change player */}
+
+    {/* add week */}
+
+    {/* add games to week */}
+
+  return (
+    <Box sx={{ width: '100%', marginTop: 4 }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Add Teams/Players
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Modify Teams
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Add Week/Games
+      </CustomTabPanel>
+    </Box>
+  );
+}
