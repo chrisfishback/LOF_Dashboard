fb4422b237e52b22c78cc920ecb8feea269aa1e5
chrisfishback <chrisfishback@icloud.com>
2024-06-01 11:07:41 -0500

    Malzahar Pun

--- front-end/src/components/Error.tsx
+++ front-end/src/components/Error.tsx
@@ -1,8 +1,8 @@
 function Error() {
     return (
         <>
-            <h1>ITS A TRAP</h1>
-            <p>Why did the Stormtrooper start a bakery? He kept wandering in the wrong direction and ended up on the dark side... of the oven!</p>
+            <h1>ENTERED THE VOID</h1>
+            <p>Why did Malzahar get promoted? Because he really knows how to 'Void' all the problems!</p>
         </>
     )
 }
