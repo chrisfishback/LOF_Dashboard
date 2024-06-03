d6ccd6145799e0a3fe5a5fb13310be9fc42a91de
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-06-03 10:12:57 -0500

    add temp data for games and their week

---
+++ src/main/resources/db/migration/V9__fill_temp_data_game_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO GAME (game_id, team1, team2, week)
+VALUES
+    ('4964711060', 'Feet Finders', 'Ornn', 1),
+    ('4964646543', 'Feet Finders', 'Ornn', 1),
+    ('4959306262', 'Feet Finders', 'First Time Gragas', 2),
+    ('4934334310', 'Feet Finders', 'Tanner Time', 3),
+    ('4934274628', 'Feet Finders', 'Tanner Time', 3)
+
