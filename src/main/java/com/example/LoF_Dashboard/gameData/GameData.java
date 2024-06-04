81135beff745bf5c875b0e255c9a98776e4da555
Christopher Fishback <christopher.fishback.mil@swf.army.mil>
2024-06-04 11:17:24 -0500

    implement game data backend

--- src/main/java/com/example/LoF_Dashboard/gameData/GameData.java
+++ src/main/java/com/example/LoF_Dashboard/gameData/GameData.java
@@ -6,13 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
 @Entity
-@Getter
-@Setter
 @Table(name = "game_data")
 public class GameData {
 
@@ -23,8 +17,8 @@ public class GameData {
     @Column(name = "game_id")
     private String gameId;
 
-    @Column(name = "summoner_id")
-    private String summonerId;
+    @Column(name = "summoner_name")
+    private String summonerName;
 
     @Column(name = "champion_name")
     private String championName;
@@ -86,9 +80,15 @@ public class GameData {
     @Column(name = "total_time_cc_dealt")
     private int totalTimeCCDealt;
 
-    public GameData(String gameId, String summonerId, String championName, String lane, int kills, int deaths, int assists, int goldEarned, int totalDamage, int towerDamage, int pentakills, int totalDamageTaken, int dragonKills, int baronKills, int towerKills, int totalTeammateHeals, int visionScore, int wardsPlaced, int wardsKilled, double killParticipation, int scuttleCrabKills, int totalTimeCCDealt) {
+    @Column(name = "total_minions_killed")
+    private int totalMinionsKilled;
+
+    @Column(name = "inhibitors_killed")
+    private int inhibitorsKilled;
+
+    public GameData(String gameId, String summonerName, String championName, String lane, int kills, int deaths, int assists, int goldEarned, int totalDamage, int towerDamage, int pentakills, int totalDamageTaken, int dragonKills, int baronKills, int towerKills, int totalTeammateHeals, int visionScore, int wardsPlaced, int wardsKilled, double killParticipation, int scuttleCrabKills, int totalTimeCCDealt, int totalMinionsKilled, int inhibitorsKilled) {
         this.gameId = gameId;
-        this.summonerId = summonerId;
+        this.summonerName = summonerName;
         this.championName = championName;
         this.lane = lane;
         this.kills = kills;
@@ -109,11 +109,13 @@ public class GameData {
         this.killParticipation = killParticipation;
         this.scuttleCrabKills = scuttleCrabKills;
         this.totalTimeCCDealt = totalTimeCCDealt;
+        this.totalMinionsKilled = totalMinionsKilled;
+        this.inhibitorsKilled = inhibitorsKilled;
     }
 
     public GameData() {
         this.gameId = "gameId";
-        this.summonerId = "summonerId";
+        this.summonerName = "summonerName";
         this.championName = "championName";
         this.lane = "lane";
         this.kills = 0;
@@ -134,5 +136,207 @@ public class GameData {
         this.killParticipation = 0.0;
         this.scuttleCrabKills = 0;
         this.totalTimeCCDealt = 0;
+        this.totalMinionsKilled = 0;
+        this.inhibitorsKilled = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getSummonerName() {
+        return summonerName;
+    }
+
+    public void setSummonerName(String summonerName) {
+        this.summonerName = summonerName;
+    }
+
+    public String getChampionName() {
+        return championName;
+    }
+
+    public void setChampionName(String championName) {
+        this.championName = championName;
+    }
+
+    public String getLane() {
+        return lane;
+    }
+
+    public void setLane(String lane) {
+        this.lane = lane;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        this.deaths = deaths;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoldEarned() {
+        return goldEarned;
+    }
+
+    public void setGoldEarned(int goldEarned) {
+        this.goldEarned = goldEarned;
+    }
+
+    public int getTotalDamage() {
+        return totalDamage;
+    }
+
+    public void setTotalDamage(int totalDamage) {
+        this.totalDamage = totalDamage;
+    }
+
+    public int getTowerDamage() {
+        return towerDamage;
+    }
+
+    public void setTowerDamage(int towerDamage) {
+        this.towerDamage = towerDamage;
+    }
+
+    public int getPentakills() {
+        return pentakills;
+    }
+
+    public void setPentakills(int pentakills) {
+        this.pentakills = pentakills;
+    }
+
+    public int getTotalDamageTaken() {
+        return totalDamageTaken;
+    }
+
+    public void setTotalDamageTaken(int totalDamageTaken) {
+        this.totalDamageTaken = totalDamageTaken;
+    }
+
+    public int getDragonKills() {
+        return dragonKills;
+    }
+
+    public void setDragonKills(int dragonKills) {
+        this.dragonKills = dragonKills;
+    }
+
+    public int getBaronKills() {
+        return baronKills;
+    }
+
+    public void setBaronKills(int baronKills) {
+        this.baronKills = baronKills;
+    }
+
+    public int getTowerKills() {
+        return towerKills;
+    }
+
+    public void setTowerKills(int towerKills) {
+        this.towerKills = towerKills;
+    }
+
+    public int getTotalTeammateHeals() {
+        return totalTeammateHeals;
+    }
+
+    public void setTotalTeammateHeals(int totalTeammateHeals) {
+        this.totalTeammateHeals = totalTeammateHeals;
+    }
+
+    public int getVisionScore() {
+        return visionScore;
+    }
+
+    public void setVisionScore(int visionScore) {
+        this.visionScore = visionScore;
+    }
+
+    public int getWardsPlaced() {
+        return wardsPlaced;
+    }
+
+    public void setWardsPlaced(int wardsPlaced) {
+        this.wardsPlaced = wardsPlaced;
+    }
+
+    public int getWardsKilled() {
+        return wardsKilled;
+    }
+
+    public void setWardsKilled(int wardsKilled) {
+        this.wardsKilled = wardsKilled;
+    }
+
+    public double getKillParticipation() {
+        return killParticipation;
+    }
+
+    public void setKillParticipation(double killParticipation) {
+        this.killParticipation = killParticipation;
+    }
+
+    public int getScuttleCrabKills() {
+        return scuttleCrabKills;
+    }
+
+    public void setScuttleCrabKills(int scuttleCrabKills) {
+        this.scuttleCrabKills = scuttleCrabKills;
+    }
+
+    public int getTotalTimeCCDealt() {
+        return totalTimeCCDealt;
+    }
+
+    public void setTotalTimeCCDealt(int totalTimeCCDealt) {
+        this.totalTimeCCDealt = totalTimeCCDealt;
+    }
+
+    public int getTotalMinionsKilled() {
+        return totalMinionsKilled;
+    }
+
+    public void setTotalMinionsKilled(int totalMinionsKilled) {
+        this.totalMinionsKilled = totalMinionsKilled;
+    }
+
+    public int getInhibitorsKilled() {
+        return inhibitorsKilled;
+    }
+
+    public void setInhibitorsKilled(int inhibitorsKilled) {
+        this.inhibitorsKilled = inhibitorsKilled;
     }
 }
--- src/main/java/com/example/LoF_Dashboard/gameData/GameDataService.java
+++ src/main/java/com/example/LoF_Dashboard/gameData/GameDataService.java
@@ -1,18 +1,77 @@
 package com.example.LoF_Dashboard.gameData;
 
-import com.example.LoF_Dashboard.game.GameRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class GameDataService {
 
+    private final RestTemplate restTemplate;
+
+    @Value("RGAPI-d6660762-3466-4c4c-a432-552e8816b0f2")
+    private String apiKey;
+
     private GameDataRepository gameDataRepository;
 
-    public GameDataService(GameDataRepository gameDataRepository) {
+    public GameDataService(GameDataRepository gameDataRepository, RestTemplate restTemplate) {
         this.gameDataRepository = gameDataRepository;
+        this.restTemplate = restTemplate;
     }
 
     public void createGameData(String gameId) {
+        String url = "https://americas.api.riotgames.com/lol/match/v5/matches/" + gameId + "?api_key=" + apiKey;
+        Map<String, Object> fetchedMatchData = getMatchData(url);
 
+        if (fetchedMatchData != null) {
+            Map<String, Object> participants = (Map<String, Object>) fetchedMatchData.get("info");
+            for (Map<String, Object> player : (List<Map<String, Object>>) participants.get("participants")) {
+                GameData gameData = parseParticipantData(player, gameId);
+                gameDataRepository.save(gameData);
+            }
+        }
+    }
+
+    private Map<String, Object> getMatchData(String url) {
+        try {
+            return restTemplate.getForObject(url, Map.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private GameData parseParticipantData(Map<String, Object> player, String gameId) {
+        GameData tempGameData = new GameData();
+        tempGameData.setGameId(gameId);
+        tempGameData.setSummonerName((String) player.get("summonerName"));
+        tempGameData.setChampionName((String) player.get("championName"));
+        tempGameData.setLane((String) player.get("lane"));
+        tempGameData.setKills(((Number) player.get("kills")).intValue());
+        tempGameData.setDeaths(((Number) player.get("deaths")).intValue());
+        tempGameData.setAssists(((Number) player.get("assists")).intValue());
+        tempGameData.setGoldEarned(((Number) player.get("goldEarned")).intValue());
+        tempGameData.setTotalDamage(((Number) player.get("totalDamageDealtToChampions")).intValue());
+        tempGameData.setTowerDamage(((Number) player.get("damageDealtToTurrets")).intValue());
+        tempGameData.setPentakills(((Number) player.get("pentaKills")).intValue());
+        tempGameData.setTotalDamageTaken(((Number) player.get("totalDamageTaken")).intValue());
+        tempGameData.setDragonKills(((Number) player.get("dragonKills")).intValue());
+        tempGameData.setBaronKills(((Number) player.get("baronKills")).intValue());
+        tempGameData.setTowerKills(((Number) player.get("turretKills")).intValue());
+        tempGameData.setTotalTeammateHeals(((Number) player.get("totalHealsOnTeammates")).intValue());
+        tempGameData.setVisionScore(((Number) player.get("visionScore")).intValue());
+        tempGameData.setWardsPlaced(((Number) player.get("wardsPlaced")).intValue());
+        tempGameData.setWardsKilled(((Number) player.get("wardsKilled")).intValue());
+        tempGameData.setKillParticipation(((Number) ((Map<String, Object>) player.get("challenges")).get("killParticipation")).doubleValue());
+        tempGameData.setScuttleCrabKills(((Number) ((Map<String, Object>) player.get("challenges")).get("scuttleCrabKills")).intValue());
+        tempGameData.setTotalTimeCCDealt(((Number) player.get("totalTimeCCDealt")).intValue());
+        tempGameData.setTotalMinionsKilled(((Number) player.get("totalMinionsKilled")).intValue());
+        tempGameData.setInhibitorsKilled(((Number) player.get("inhibitorKills")).intValue());
+        return tempGameData;
     }
 }
